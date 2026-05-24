4a129780c45f77ee4d77bf00fb08373bf81e412b
Centraleholy <163032384+Centraleholy@users.noreply.github.com>
2026-05-24 15:05:21 +0200

    Add service worker for BigSlame Academy PWA
    Implement service worker for caching and offline support.

---
+++ public/sw.js
@@ -0,0 +1,37 @@
+// Service Worker pour BigSlame Academy PWA
+const CACHE_NAME = 'bigslame-v1';
+const urlsToCache = [
+  '/',
+  '/index.html'
+];
+
+// Installation
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => cache.addAll(urlsToCache))
+  );
+});
+
+// Activation
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
+
+// Fetch
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => response || fetch(event.request))
+  );
+});
